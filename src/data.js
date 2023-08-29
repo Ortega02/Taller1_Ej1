5321a937a0c77552e7991dde8d0a6218949b396a
Ortega02 <io.ortega.tony@gmail.com>
2023-08-29 08:57:30 -0600

    Comenzar ejercicio

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const data=[
+    {
+    id:1,
+    title:'La Sirenita',
+    price:100,
+    urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity:1
+    },
+    {
+    id:2,
+    title:'Barbie',price:190,
+    urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity:1
+    },
+    {
+    id:3,
+    title:'Tierra de osos',
+    price:30,
+    urlImage:'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+    quantity:1
+    },
+    {
+    id:4,
+    title:'Rapidos y Furiosos',
+    price:30,
+    urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+    quantity:1
+    }, {
+    id:5,
+    title:'Harry Potter y La Orden del Fenix',
+    price:390,
+    urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+    quantity:1
+    }
+    ];
